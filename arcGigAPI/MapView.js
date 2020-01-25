565c6b50c6c86a4c68f39692acda136d404aeadb
riyadh <riyadh02220@gmail.com>
2020-01-25 10:01:07 +0100

    Make a mark on long press

--- arcGigAPI/MapView.js
+++ arcGigAPI/MapView.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import  MapView  from 'react-native-maps';
 import { Marker } from 'react-native-maps';
-
 import { StyleSheet, Text, View, Dimensions, Button, TouchableOpacity} from 'react-native';
-//import { TouchableOpacity } from 'react-native-gesture-handler';
-//import { Button } from 'react-native-paper';
 
 export default class App extends React.Component {
 
@@ -16,8 +13,17 @@ export default class App extends React.Component {
         longitude: 6.61472,
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421,
-      }
+      },
+      latLng : {
+        latitude: 0,
+        longitude: 0,
+      },
     }
+    this._makeMarker = this._makeMarker.bind(this)
+  }
+
+  _makeMarker(e) {
+    this.setState({latLng : e.nativeEvent.coordinate})
   }
 
 
@@ -29,6 +35,13 @@ export default class App extends React.Component {
           style={styles.mapStyle} 
           onLongPress={this._makeMarker}
         >
+        <Marker
+          draggable
+          coordinate={this.state.latLng}
+          title={"valle"}
+          description={"oued rhumel"}
+          onDragEnd={(e) => this.setState({ x: e.nativeEvent.coordinate })}
+        ></Marker>
       </MapView>
     );
   }
