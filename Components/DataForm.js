2fa65044957a5ecd605ecacb73461d4d8b8ddd3d
riyadh <riyadh02220@gmail.com>
2020-02-28 08:38:04 +0100

    output view added

--- Components/DataForm.js
+++ Components/DataForm.js
@@ -5,29 +5,46 @@ class Data extends React.Component {
     constructor(props){
         super(props)
         this.state = {
-
+          input : "test",
         }
     }
+
 
     
     render() {
       return(
+        <View style={styles.dataContainer}>
+          
+          <View style={styles.output}>
+            <Text>{this.state.input}</Text>
+            <Text>{this.state.input}</Text>
+          </View>
+
           <View style={styles.table}>
-            <Text>dfdfdff</Text>
-            <Text>dfdfdff</Text>
-            <Text>dfdfdff</Text>
-            <Text>dfdfdff</Text>
             <TextInput
               style={styles.input}
               placeholder={"test"}
-              enablesReturnKeyAutomatically={true}
+            ></TextInput>
+            <TextInput
+              style={styles.input}
+              placeholder={"test"}
             ></TextInput>
           </View>
+        </View>
+
       )
     }
 }
 
 const styles = StyleSheet.create({
+  dataContainer: {
+    position: "absolute",
+    width: (Dimensions.get('window').width) ,
+    height: (Dimensions.get('window').height),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
   table: {
     position: "absolute",
     top: (Dimensions.get('window').height) * 0.5,
@@ -47,6 +64,19 @@ const styles = StyleSheet.create({
       borderColor: 'gray', 
       borderWidth: 1 ,
       borderRadius: 15,
+  },
+  output: {
+    position: "absolute",
+    top: (Dimensions.get('window').height) * 0.25,
+    left: (Dimensions.get('window').height) * 0.025,
+    width: (Dimensions.get('window').width) * 0.9,
+    height: (Dimensions.get('window').height) * 0.2,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+
+    backgroundColor: "green",
+    borderRadius: 20,
   }
 })
 
